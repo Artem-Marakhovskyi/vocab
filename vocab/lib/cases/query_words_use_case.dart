1355f425f4c4aec8126bfbc86eb04e960cd54d7a
Artem Marakhovskyi <Artem.marakhovskyi@gmail.com>
2022-07-27 17:44:46 +0200

    query multiple words use case

--- vocab/lib/cases/query_words_use_case.dart
+++ vocab/lib/cases/query_words_use_case.dart
@@ -1,11 +1,16 @@
+import 'package:vocab/cases/query_word_use_case.dart';
 import 'package:vocab/cases/use_case.dart';
 
+import 'args/args.dart';
+
 class QueryWordsUseCase extends UseCase {
+  QueryWordsArgs get arguments => args as QueryWordsArgs;
   QueryWordsUseCase(super.args);
 
   @override
-  Future execute() {
-    // TODO: implement execute
-    throw UnimplementedError();
+  Future execute() async {
+    for (var word in arguments.words) {
+      await QueryWordUseCase(QueryWordArgs(word)).execute();
+    }
   }
 }
